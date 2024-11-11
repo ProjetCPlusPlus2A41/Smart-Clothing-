55a13b1229c52ef13e13c369155d017f28424d73
RAMZGHARBI <165672378+RAMZGHARBI@users.noreply.github.com>
2024-11-11 01:40:38 +0100

    First commit all files

---
+++ connection.cpp
@@ -0,0 +1,24 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("YASMINE");//inserer le nom de la source de données
+db.setUserName("ramz");//inserer nom de l'utilisateur
+db.setPassword("ramz");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
+void Connection::closeConnect(){ db.close();}
